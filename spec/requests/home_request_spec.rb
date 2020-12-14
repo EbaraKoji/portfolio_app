815256d45ed928d69885f8d391e40483a3845990
EbaraKojiOrg <kojieva5270@gmail.com>
2020-12-14 20:40:30 +0900

    Add page_titles

--- spec/requests/home_request_spec.rb
+++ spec/requests/home_request_spec.rb
@@ -2,10 +2,13 @@ require 'rails_helper'
 
 RSpec.describe "Homes", type: :request do
 
+  let(:base_title) { 'Portfolio App' }
+
   describe "GET /top" do
     it "returns http success" do
       get root_url
       expect(response).to have_http_status(:success)
+      assert_select "title", "Top | #{base_title}"
     end
   end
 
@@ -13,6 +16,7 @@ RSpec.describe "Homes", type: :request do
     it "returns http success" do
       get help_url
       expect(response).to have_http_status(:success)
+      assert_select "title", "Help | #{base_title}"
     end
   end
 
@@ -20,6 +24,7 @@ RSpec.describe "Homes", type: :request do
     it "returns http success" do
       get about_url
       expect(response).to have_http_status(:success)
+      assert_select "title", "About | #{base_title}"
     end
   end
 

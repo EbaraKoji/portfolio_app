4c2369bbff033bbeb64f30d60fe5eeb0edc37ff9
EbaraKojiOrg <kojieva5270@gmail.com>
2020-12-14 14:19:22 +0900

    Set links in home page

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,6 @@
 Rails.application.routes.draw do
   root 'home#top'
-  get 'home/top'
-  get 'home/help'
-  get 'home/about'
+  get  '/help',    to: 'home#help'
+  get  '/about',   to: 'home#about'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end

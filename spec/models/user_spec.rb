02fce1485b2f1f1de76a7e621224bd54db525020
EbaraKojiOrg <kojieva5270@gmail.com>
2020-12-17 15:58:10 +0900

    Write basic User model test

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,34 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+
+  describe '#signup' do
+    before do
+      @user = build(:user)
+    end
+    it "email should be present" do
+      @user.email = nil
+      expect(@user).not_to be_valid
+      expect(@user.errors[:email]).to include("が入力されていません。")
+    end
+  
+    it "password should be present" do
+      @user.password = nil
+      expect(@user).not_to be_valid
+      expect(@user.errors[:password]).to include("が入力されていません。")
+    end
+    
+    it "password should be at least 6 letters" do
+      @user.password = 'aaaaa'
+      expect(@user).not_to be_valid
+      expect(@user.errors[:password]).to include("は6文字以上に設定して下さい。")
+    end
+  
+    it "password and password_confirmation should be same" do
+      @user.password = 'password1'
+      @user.password_confirmation = 'password2'
+      expect(@user).not_to be_valid
+      expect(@user.errors[:password_confirmation]).to include("が一致しません。")
+    end
+  end
 end
